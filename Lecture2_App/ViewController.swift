6021b6a70866b38a547d85ba7255a9aaee162895
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-27 00:47:27 -0400

    Initial Commit

---
+++ Lecture2_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lecture2_App
+//
+//  Created by Cristóbal Contreras on 27-08-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lecture2_AppUITests/Lecture2_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lecture2_AppUITestsLaunchTests.swift
+//  Lecture2_AppUITests
+//
+//  Created by Cristóbal Contreras on 27-08-23.
+//
+
+import XCTest
+
+final class Lecture2_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
